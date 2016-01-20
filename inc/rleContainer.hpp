228b370a7934acc882bacc27ea855d1997d1b908
Tastyep <luc.sinet@epitech.eu>
2016-01-20 21:41:02 +0100

    Used clang-format to beautify the code

--- inc/rleContainer.hpp
+++ inc/rleContainer.hpp
@@ -4,98 +4,76 @@
 #include <vector>
 #include <iostream>
 
-template <typename T>
-struct	rleData
-{
-  T		data;
-  unsigned int	count;
+template <typename T> struct rleData {
+  T data;
+  unsigned int count;
 
-  rleData(const T &d, unsigned c) :
-    data(d), count(c)
-  {
-  }
+  rleData(const T &d, unsigned c) : data(d), count(c) {}
 };
 
 enum class blockPlacement { Align, Fragment };
 
-template <typename T>
-class rleContainer
-{
+template <typename T> class rleContainer {
 public:
   rleContainer();
   ~rleContainer() = default;
 
-  void		push_back(const T &val);
-  std::size_t	memory_used() const;
-  T		&operator[](unsigned int idx);
+  void push_back(const T &val);
+  std::size_t memory_used() const;
+  T &operator[](unsigned int idx);
 
 private:
-  bool	tryMerge(std::vector<T> &vec, const T &val);
+  bool tryMerge(std::vector<T> &vec, const T &val);
 
 private:
-  std::vector<rleData<T> >	_aligned;
-  std::vector<std::vector<T> >	_fragmented;
-  std::vector<unsigned int>	_links;
-  blockPlacement		_last;
+  std::vector<rleData<T>> _aligned;
+  std::vector<std::vector<T>> _fragmented;
+  std::vector<unsigned int> _links;
+  blockPlacement _last;
 };
 
 template <typename T>
-rleContainer<T>::rleContainer() :
-  _last(blockPlacement::Fragment)
-{
+rleContainer<T>::rleContainer() : _last(blockPlacement::Fragment) {}
+
+template <typename T> void rleContainer<T>::push_back(const T &val) {
+  if (_last == blockPlacement::Fragment) {
+    if (!_fragmented.empty()) {
+      std::vector<T> &vec = _fragmented.back();
+
+      if (!vec.empty() && vec.back() == val) {
+        if (tryMerge(vec, val) == false)
+          vec.push_back(val);
+        else
+          _last = blockPlacement::Align;
+      } else
+        vec.push_back(val);
+    } else // means it's the first call
+    {
+      _fragmented.push_back({val});
+      _links.push_back(_aligned.size());
+    }
+  } else {
+    if (_aligned.back().data == val)
+      ++_aligned.back().count;
+    else {
+      _fragmented.push_back({val});
+      _links.push_back(_aligned.size());
+      _last = blockPlacement::Fragment;
+    }
+  }
 }
 
 template <typename T>
-void	rleContainer<T>::push_back(const T &val)
-{
-  if (_last == blockPlacement::Fragment)
-    {
-      if (!_fragmented.empty())
-	{
-	  std::vector<T>	&vec = _fragmented.back();
+bool rleContainer<T>::tryMerge(std::vector<T> &vec, const T &val) {
+  unsigned int counter = 1;
+  unsigned int gain = 0;
 
-	  if (!vec.empty() && vec.back() == val)
-	    {
-	      if (tryMerge(vec, val) == false)
-		vec.push_back(val);
-	      else
-		_last = blockPlacement::Align;
-	    }
-	  else
-	    vec.push_back(val);
-	}
-      else // means it's the first call
-	{
-	  _fragmented.push_back({val});
-	  _links.push_back(_aligned.size());
-	}
-    }
-  else
-    {
-      if (_aligned.back().data == val)
-	++_aligned.back().count;
-      else
-	{
-	  _fragmented.push_back({val});
-	  _links.push_back(_aligned.size());
-	  _last = blockPlacement::Fragment;
-	}
-    }
-}
-
-template <typename T>
-bool	rleContainer<T>::tryMerge(std::vector<T> &vec, const T &val)
-{
-  unsigned int	counter = 1;
-  unsigned int	gain = 0;
-
-  for (auto rit = vec.rbegin() + 1; rit != vec.rend(); ++rit)
-    {
-      if (*rit == val)
-	++counter;
-      else
-	break;
-    }
+  for (auto rit = vec.rbegin() + 1; rit != vec.rend(); ++rit) {
+    if (*rit == val)
+      ++counter;
+    else
+      break;
+  }
 
   if (counter == vec.size()) // means it will be emptyed
     gain += sizeof(unsigned int);
@@ -103,64 +81,54 @@ bool	rleContainer<T>::tryMerge(std::vector<T> &vec, const T &val)
     _aligned.push_back(rleData<T>(val, counter + 1));
   else
     return false;
-  if (gain)
-    {
-      _fragmented.pop_back();
-      _links.pop_back();
-    }
-  else
-    {
-      for (unsigned int remover = 0; remover < counter; ++remover)
-	vec.pop_back();
-    }
+  if (gain) {
+    _fragmented.pop_back();
+    _links.pop_back();
+  } else {
+    for (unsigned int remover = 0; remover < counter; ++remover)
+      vec.pop_back();
+  }
   return true;
 }
 
-template <typename T>
-T	&rleContainer<T>::operator[](unsigned int idx)
-{
-  unsigned int	counter = 0;
-  unsigned int	linkIdx = 0;
-  unsigned int	alignIdx = 0;
-  unsigned int	nextlink = -1;
+template <typename T> T &rleContainer<T>::operator[](unsigned int idx) {
+  unsigned int counter = 0;
+  unsigned int linkIdx = 0;
+  unsigned int alignIdx = 0;
+  unsigned int nextlink = -1;
 
   if (!_links.empty())
     nextlink = _links[linkIdx];
-  while (counter <= idx)
-    {
-      if (alignIdx == nextlink)
-	{
-	  std::vector<T>	&vec = _fragmented[linkIdx++];
-	  int			pos = idx - counter;
+  while (counter <= idx) {
+    if (alignIdx == nextlink) {
+      std::vector<T> &vec = _fragmented[linkIdx++];
+      int pos = idx - counter;
 
-	  if (pos < vec.size())
-	    return (vec[pos]);
-	  counter += vec.size();
-	  if (linkIdx < _links.size())
-	    nextlink = _links[linkIdx];
-	  else
-	    nextlink = -1;
-	}
+      if (pos < vec.size())
+        return (vec[pos]);
+      counter += vec.size();
+      if (linkIdx < _links.size())
+        nextlink = _links[linkIdx];
       else
-	{
-	  int	pos = idx - counter;
+        nextlink = -1;
+    } else {
+      int pos = idx - counter;
 
-	  if (pos < _aligned[alignIdx].count)
-	    return _aligned[alignIdx].data;
-	  counter += _aligned[alignIdx].count;
-	  ++alignIdx;
-	}
+      if (pos < _aligned[alignIdx].count)
+        return _aligned[alignIdx].data;
+      counter += _aligned[alignIdx].count;
+      ++alignIdx;
     }
+  }
 }
 
-template <typename T>
-std::size_t	rleContainer<T>::memory_used() const
-{
-  std::size_t	fragmentSize = 0;
+template <typename T> std::size_t rleContainer<T>::memory_used() const {
+  std::size_t fragmentSize = 0;
 
   for (auto vec : _fragmented)
     fragmentSize += vec.size() * sizeof(T);
-  return _aligned.size() * sizeof(rleData<T>) + fragmentSize + _links.size() * sizeof(unsigned int);
+  return _aligned.size() * sizeof(rleData<T>) + fragmentSize +
+         _links.size() * sizeof(unsigned int);
 }
 
 #endif /* _RLECONTAINER_H_ */
--- main.cpp
+++ main.cpp
@@ -3,30 +3,28 @@
 #include <ctime>
 #include "rleContainer.hpp"
 
-int main(int argc, char *argv[])
-{
-  rleContainer<unsigned int>	rle;
-  unsigned int	counter = 2000;
-  std::vector<unsigned int>	vec;
+int main(int argc, char *argv[]) {
+  rleContainer<unsigned int> rle;
+  unsigned int counter = 2000;
+  std::vector<unsigned int> vec;
 
   std::srand(std::time(0));
-  for (unsigned int i = 0; i < counter; ++i)
-    {
-      unsigned int nb = std::rand() % 100;
-      if (nb > 75)
-	nb = 1;
-      else
-	nb = 0;
-      // std::cout << nb;
-      vec.push_back(nb);
-      rle.push_back(nb);
-    }
+  for (unsigned int i = 0; i < counter; ++i) {
+    unsigned int nb = std::rand() % 100;
+    if (nb > 50)
+      nb = 1;
+    else
+      nb = 0;
+    // std::cout << nb;
+    vec.push_back(nb);
+    rle.push_back(nb);
+  }
   // std::cout << std::endl;
   // for (unsigned int i = 0; i < counter; ++i)
   //   std::cout << rle[i];
   // std::cout << std::endl;
-  std::cout << "memory used: " << counter * sizeof(unsigned int)  << " vs "
-	    << rle.memory_used() << std::endl;
+  std::cout << "memory used: " << counter * sizeof(unsigned int) << " vs "
+            << rle.memory_used() << std::endl;
   // for (auto &i : vec)
   //   std::cout << i;
   // std::cout << std::endl;
